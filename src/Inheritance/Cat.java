3537891492035aa8bee8bfc266780fe965fdd735
Wong <allison.wong@fiserv.com>
2023-10-27 13:29:30 -0400

    Inheritance Java basics II:

---
+++ src/Inheritance/Cat.java
@@ -0,0 +1,23 @@
+package Inheritance;
+public class Cat {
+
+    // Data...
+    private String name;
+
+    // Constructors...
+    public Cat(String name){
+        this.name = name;
+    }
+
+    // Methods...
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/Inheritance/Main.java
@@ -0,0 +1,7 @@
+package Inheritance;
+public class Main{
+    public static void main(String[] args) {
+        var myCat = new Cat("Gypsy");
+        System.out.println(myCat.toString());
+    }
+}
